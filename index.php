1c95613a923b4e7f92424f9a7aa26591d07cfb38
jomarx <jmaquinay@gmail.com>
2018-03-09 22:00:15 +0800

    start page

---
+++ index.php
@@ -0,0 +1,52 @@
+<html>
+<head>
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script src="js/submit.js"></script>
+</head>
+<?php
+$page = $_SERVER['PHP_SELF'];
+$sec = "3600";
+include("config.php");
+session_start();
+error_reporting(0);
+
+if ($_SESSION["id"]!=""){
+	header('location:home.php');
+}
+
+?>
+<body>
+<b><center><br><img src="hb.jpg"><br>
+<p> Hanesbrands Philippines Inc<br><br><br>Daily Sales Report Database<br></p>
+<BR><a href="csvLoad.php">CSV Loader</a><BR><BR>
+<BR><a href="csvView.php">CSV ALL Viewer</a><BR><BR>
+<BR><a href="dsr_month.php">DSR Monthly Generator</a><BR><BR>
+<br>
+<br>
+
+<script>
+$('#myform').submit(function(){
+ return false;
+});
+ 
+$('#submit').click(function(){
+
+
+ $.post( 
+ $('#myform').attr('action'),
+ $('#myform :input').serializeArray(),
+ function(result){
+ $('#result1').html(result);
+ $('#myform')[0].reset();
+ }
+ );
+
+});
+
+</script>
+</form>
+<font color="red">
+<div id="result1"></div>
+</font></center></b>
+</body>
+</html>
